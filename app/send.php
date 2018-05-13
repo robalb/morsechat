085c6a72bb406340a2b77111d4cbe38545675946
robalb <alberto.ventafridda@gmail.com>
2018-05-13 18:05:23 +0200

    php code refactoring and fixed bugs
    - fixed bug that allows the transmission of empty messages
    - fixed critical bug: the error string 'wait $n seconds' is in plural form even when the second is only one

--- app/send.php
+++ app/send.php
@@ -1,62 +1,101 @@
 <?php
-/*
-this script receives an ajax call from the connected users when they send a message.
-received data: msg (get)
-if the session is valid, the message is broadcasted.
-
+/**
+* this script receives an ajax call from the connected users when they send a message.
+* received data: msg (get)
+* if the session is valid, the message is broadcasted.
+* 
 */
+
 session_start();
 require_once('pusher/Pusher.php');
 require_once('pusher/PusherException.php');
 require_once('pusher/PusherInstance.php');
 
-$config = include('config.php');
+/*
+* @global array $CONFIG - an array containig all the configuration data
+*   $CONFIG = [
+*    'APP_KEY'        => (string) pusher app key,
+*    'APP_SECRET'     => (string) pusher app secret,
+*    'APP_ID'         => (string) pusher app id,
+*    'APP_CLUSTER'    => (string) pusher cluster country code,
+*
+*    'ADMIN_USERNAME' => (string) admin username,
+*    'ADMIN_PASSWORD' => (string) admin bcript password hash,
+*   
+*    'MAX_CHANNELS'   => (integer) max channels aviable ,
+*    'MSG_COOLDOWN'   => (integer) seconds of cooldown before a user can send messages
+*   ]
+*/
+$CONFIG = include('config.php');
 
-
-  $options = array(
-	'cluster' => $config['APP_CLUSTER'],
-	'encrypted' => true
-  );
-
-  $pusher = new Pusher\Pusher(
-	$config['APP_KEY'],
-	$config['APP_SECRET'],
-	$config['APP_ID'],
-	$options
-  );
-//validate session
-if(isset($_SESSION["user_id"]) && isset($_SESSION["channel"]) ){
-	//validate anti-spam cooldown
-	if(!isset($_SESSION["last_msg"]) || (time() - $_SESSION["last_msg"] > $config['MSG_COOLDOWN']) ){
-		//validate msg
-		//TODO: remove spaces (J) at the beginnig of the msg
-		if(isset($_GET["msg"]) && strlen($_GET["msg"]) > 0 && strlen($_GET["msg"]) < 400 && preg_match('/^[a-zA-Z0-9]+$/', $_GET["msg"]) ){
-			//removes double spaces
-			$msg = str_replace("JJ","J",$_GET["msg"]);
-			//removes initial spaces
-			$msg = ltrim($msg);
-			$data['message'] = $msg;
-			$data['sender'] = $_SESSION["user_id"];
-			$data['time'] = time();
-			$pusher->trigger($_SESSION["channel"], 'morsebroadcast', $data);
-			$_SESSION["last_msg"] = time();
-			echo "sent!";
-			$_SESSION["keying"] = false;
-		}else{
-			header("HTTP/1.0 400 invalid string");
-			echo "invalid string";
-		}
-	}else{
-		$seconds = $config['MSG_COOLDOWN'] - (time() - $_SESSION["last_msg"]);
-		//TODO: change status to 400
-		header("HTTP/1.0 403 wait ".$seconds." seconds");
-		echo "wait ".$seconds." seconds";
-	}
-}else{
-	header("HTTP/1.0 401 invalid session");
-	echo "invalid session";
+/**
+* this function validates the msg recived
+* 
+* @params string $msg - the msg to validate
+*
+* @return boolean
+*/
+function validateMsg($msg){
+    //removes double spaces
+    $msg = str_replace("JJ","J",$_GET["msg"]);
+    //removes initial spaces
+    $msg = ltrim($msg,"J");
+    
+    $valid_length = (strlen($msg) > 0 && strlen($msg) < 400);
+    $valid_format = preg_match('/^[a-zA-Z0-9]+$/', $msg);
+    return ($valid_length && $valid_format);
 }
- 
-  
-  
+
+
+if(!isset($_SESSION["user_id"]) || !isset($_SESSION["channel"]) ){
+    //user is not logged
+    header("HTTP/1.0 401 invalid session");
+    echo "invalid session";    
+}
+else if(isset($_SESSION["last_msg"]) && (time() - $_SESSION["last_msg"] < $CONFIG['MSG_COOLDOWN']) ){
+    //user is messaging too quickly
+    $seconds = $CONFIG['MSG_COOLDOWN'] - (time() - $_SESSION["last_msg"]);
+    $plural = ($seconds === 1)?"":"s";
+    header("HTTP/1.0 403 wait ".$seconds." second".$plural);
+    echo "wait ".$seconds." second".$plural; 
+}
+else if(!isset($_GET["msg"]) ){
+    //msg not found
+    header("HTTP/1.0 403 invalid parameters");
+    echo "invalid parameters";
+}
+else if(!validateMsg($_GET["msg"]) ){
+    //invalid msg
+    header("HTTP/1.0 403 invalid msg");
+    echo "invalid msg";      
+}
+else{
+    //all good. broadcast the message
+    $options = array(
+        'cluster' => $CONFIG['APP_CLUSTER'],
+        'encrypted' => true
+    );
+    $pusher = new Pusher\Pusher(
+        $CONFIG['APP_KEY'],
+        $CONFIG['APP_SECRET'],
+        $CONFIG['APP_ID'],
+        $options
+    );
+    //removes double spaces
+    $msg = str_replace("JJ","J",$_GET["msg"]);
+    //removes initial spaces
+    $msg = ltrim($msg,"J");
+    
+    $data = array(
+        'message' => $msg,
+        'sender' => $_SESSION["user_id"],
+        'time' => time()
+    );
+    $pusher->trigger($_SESSION["channel"], 'morsebroadcast', $data);
+    echo "sent!";
+    //update session variables
+    $_SESSION["last_msg"] = time();
+    $_SESSION["keying"] = false;
+}
+
 ?>
