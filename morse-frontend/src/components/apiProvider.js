2ac230bce571b266ebec00e985d5980645062392
robalb <alberto.ventafridda@gmail.com>
2021-10-08 01:41:13 +0200

    cleaned code, separated providers

---
+++ morse-frontend/src/components/apiProvider.js
@@ -0,0 +1,95 @@
+import * as React from "react"
+
+
+import {request} from '../utils/apiResolver'
+import mainContext from '../contexts/mainContext'
+import { useAlert } from "react-alert";
+
+
+const ApiProvider = ({children}) => {
+  const alert = useAlert();
+  /*
+   * the main app state
+   */
+  let [state, setState] = React.useState({
+      logged: false,
+      loading: true,
+      userData: {}
+  })
+
+  React.useEffect(()=>{
+    alertError("asd asdas")
+  }, [])
+
+
+  //internal states
+  let [apiState, setApiState] = React.useState({
+    csrf: ""
+  })
+  //TODO: work on this hardcoded url
+  let baseUrl = 'http://localhost:5000/api/v1/'
+
+  /*
+  * makes a special api call to the backend to get the csrf protection token,
+  * and stores it in the apiState
+  */
+  async function initCsrfToken(){
+    let url = baseUrl + 'csrf'
+    let call = await request(url, {})
+    if( call.success && call.data.token )
+      setApiState(state => ({
+        ...state,
+        csrf: call.data.token
+      }))
+    else
+      console.log("csrfToken init failed")
+    return call
+  }
+  /*
+   * api call to a specific api endpoint
+   * init the csrf token if not set
+   */
+  async function post(endpoint, data){
+    let csrf = apiState.csrf
+    //if the csrf token hasn't been initialized yet
+    if(csrf.length == 0){
+      let initCall = await initCsrfToken()
+      if(!initCall.success){
+        alertError("operation failed, please retry. " + initCall.error)
+        return {
+          success: false,
+          error: "csrf_init_failed",
+          details: initCall.details
+        }
+      }
+      else{
+        csrf = initCall.data.token
+      }
+    }
+    //make request, on fail alert error
+    let response = await request(baseUrl + endpoint, data, csrf)
+    if(!response.success)
+      alertError("operation failed, please retry. " + response.error)
+    return response
+  }
+
+  function alertError(error){
+    alert.error(error)
+  }
+
+
+  let mainContextValues = {
+    state,
+    setState,
+    post
+  }
+
+ return(
+      <mainContext.Provider value={mainContextValues}>
+        {children}
+      </mainContext.Provider>
+ )
+}
+
+export default ApiProvider
+
--- morse-frontend/src/components/providers.js
+++ morse-frontend/src/components/providers.js
@@ -2,10 +2,7 @@ import * as React from "react"
 
 import { positions, Provider} from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
-
-import {request} from '../utils/apiResolver'
-import mainContext from '../contexts/mainContext'
-import AlertProviderBounce from './alertProviderBounce'
+import ApiProvider from './apiProvider'
 
 const alertTemplateOptions = {
   timeout: 5000,
@@ -26,116 +23,17 @@ const alertTemplateOptions = {
 //   </div>
 // )
 
+/*
+ * this component wraps all the providers toghether into one single
+ * provider HOC
+ */
 const Providers = ({children}) => {
-  /*
-   * the main app state
-   */
-  let [state, setState] = React.useState({
-      logged: false,
-      loading: true,
-      userData: {}
-  })
-
-  React.useEffect(()=>{
-    alertError("asd asdas")
-  }, [])
-
-
-  //internal states
-  let [apiState, setApiState] = React.useState({
-    csrf: ""
-  })
-  let [alertState, setAlertState] = React.useState({
-    alertRef: false,
-    queque: []
-  })
-  //TODO: work on this hardcoded url
-  let baseUrl = 'http://localhost:5000/api/v1/'
-
-  /*
-  * makes a special api call to the backend to get the csrf protection token,
-  * and stores it in the apiState
-  */
-  async function initCsrfToken(){
-    let url = baseUrl + 'csrf'
-    let call = await request(url, {})
-    if( call.success && call.data.token )
-      setApiState(state => ({
-        ...state,
-        csrf: call.data.token
-      }))
-    else
-      console.log("csrfToken init failed")
-    return call
-  }
-  /*
-   * api call to a specific api endpoint
-   * init the csrf token if not set
-   */
-  async function post(endpoint, data){
-    let csrf = apiState.csrf
-    //if the csrf token hasn't been initialized yet
-    if(csrf.length == 0){
-      let initCall = await initCsrfToken()
-      if(!initCall.success){
-        alertError("operation failed, please retry. " + initCall.error)
-        return {
-          success: false,
-          error: "csrf_init_failed",
-          details: initCall.details
-        }
-      }
-      else{
-        csrf = initCall.data.token
-      }
-    }
-    //make request, on fail alert error
-    let response = await request(baseUrl + endpoint, data, csrf)
-    if(!response.success)
-      alertError("operation failed, please retry. " + response.error)
-    return response
-  }
-
-  function alertError(error){
-    setAlertState(s => {
-      let newQueque = s.queque
-      if(s.alertRef){
-        s.alertRef.error(error)
-      }else{
-        newQueque.push(error)
-      }
-      return {...s, queque:newQueque}
-
-    })
-  }
-  function setAlertRef(ref){
-    if(alertState.alertRef == false)
-      setAlertState(s =>{
-        if(s.queque.length > 0){
-          s.queque.forEach(msg => ref.errror(msg))
-        }
-        return {
-          ...s,
-          alertRef: ref,
-          queque: []
-        }
-      })
-  }
-
-
-  let mainContextValues = {
-    state,
-    setState,
-    post
-  }
-
  return(
-      <mainContext.Provider value={mainContextValues}>
       <Provider template={AlertTemplate} {...alertTemplateOptions}>
-        <AlertProviderBounce passAlertRef={setAlertRef}></AlertProviderBounce>
+        <ApiProvider>
         {children}
+        </ApiProvider>
       </Provider>
-      </mainContext.Provider>
  )
 }
 
