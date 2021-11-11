34087e18983111662365ba4a2b4dac342e706f06
robalb <alberto.ventafridda@gmail.com>
2021-11-11 18:26:26 +0100

    integrting websocket

--- morse-frontend/src/pages/chat.js
+++ morse-frontend/src/pages/chat.js
@@ -2,11 +2,33 @@ import React from 'react';
 import Providers from '../components/providers'
 import mainContext from '../contexts/mainContext'
 import Seo from "../components/seo"
+import {io} from 'socket.io-client'
 
 import '../styles/common.css'
+import MainDataLoading from '../components/mainDataLoading';
 
 function Index(props){
-    return <><p>hello</p></>
+    let { state, post, reload } = React.useContext(mainContext)
+
+    React.useEffect(() => {
+
+        const socket = io('http://localhost:5000');
+
+        // client-side
+        socket.on("connect", () => {
+            console.log("connected")
+            console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+        });
+        socket.on("disconnect", () => {
+            console.log("disconnected")
+            console.log(socket.id); // undefined
+        });
+
+    }, [])
+
+    return <>
+        <p> {state.sessionData.authenticated ? "authenticated" : "--"} </p>
+    </>
 }
 
 const ChatPage = () => {
