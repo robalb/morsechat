4489a44677eaa0733a19744b0da55b178913a517
robalb <alberto.ventafridda@gmail.com>
2022-02-07 00:49:52 +0100

    ported dependencies, fixed import path

--- morse-vite/src/pages/App.jsx
+++ morse-vite/src/pages/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
-        index
+        index2
         </Typography>
         <ProTip />
         <Copyright />
