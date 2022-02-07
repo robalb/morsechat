88173fc2186e4a4337599bf1cef7ff20b46063ef
robalb <alberto.ventafridda@gmail.com>
2022-02-07 01:09:09 +0100

    added snackbar and api providers. both untested

--- morse-vite/src/pageRender/pageRender.jsx
+++ morse-vite/src/pageRender/pageRender.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme';
-
+import ApiProvider from './apiProvider'
+import { SnackbarProvider } from 'notistack';
 import './base.css'
 
 export default function render(App){
@@ -12,7 +13,17 @@ export default function render(App){
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
-        <App />
+        <SnackbarProvider 
+          anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+          }}
+          maxSnack={3}
+          >
+          <ApiProvider>
+            <App />
+          </ApiProvider>
+        </SnackbarProvider>
       </ThemeProvider>,
     </React.StrictMode>,
     document.getElementById('root')
