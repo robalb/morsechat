93a5000f2ed4140e0b01b33f492b810940ee1e1c
robalb <alberto.ventafridda@gmail.com>
2022-02-11 16:05:20 +0100

    moved everything to dedicated components, preparing for code refactor

---
+++ morse-vite/src/components/info/Info.jsx
@@ -0,0 +1,25 @@
+import discordUrl from "../../fonts/discord.svg";
+import githubUrl from "../../fonts/github.svg";
+import * as React from "react";
+import './info.css'
+
+export function Info({className="", ...props}) {
+    return (
+        <div className={`info ${className}`} >
+            <h1>Morsechat</h1>
+            <p>Welcome to the best internet morse radio. </p>
+            <p>
+                This webapp is open source! <br/> Join the discord community
+                to get the latest news, and to participate on its development.
+            </p>
+            <h3>What's new</h3>
+            <p>2021-02-10 update notes</p>
+            <a href="https://discord.gg/JPzfzNJG6e">
+                <img src={discordUrl}/>
+            </a><br/>
+            <a href="https://github.com/robalb/morsechat">
+                <img src={githubUrl}/>
+            </a>
+        </div>
+    )
+}
--- morse-vite/src/pages/chat/App.jsx
+++ morse-vite/src/pages/chat/App.jsx
@@ -14,16 +14,15 @@ import CloseIcon from '@mui/icons-material/Close';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
 import VolumeOffIcon from '@mui/icons-material/VolumeOff';
-import MoreVertIcon  from '@mui/icons-material/MoreVert'
 
 import pageRender from '../../pageRender/pageRender'
 
 import './app.css'
-import discordUrl from '../../fonts/discord.svg'
-import githubUrl from '../../fonts/github.svg'
-import blur from '../../fonts/blur.png'
+import {Info} from "../../components/info/Info";
 
-export default function App() {
+
+
+function Header(props){
   let [sidebarOpen, setSidebarOpen] = React.useState(false);
   function toggleSidebar(event){
     if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
@@ -31,8 +30,7 @@ export default function App() {
     }
     setSidebarOpen(false);
   }
-  return (
-    <div className="app-container">
+  return(
     <header>
 
       <div>
@@ -73,112 +71,135 @@ export default function App() {
       </div>
 
     </header>
-    <main>
-      <div className="side">
-          <div className="online">
-            <h2>online</h2>
-          {
-            ['IT000HAL', 'AS89ASD', 'ASDASDd', 'SLUR000'].map((h, i) =>
-              <div key={i}>
-                <div className="left">
-                  <p>{h}</p>
-                  <div className="typing">
-                    <div></div>
-                    <div></div>
-                  </div>
-                </div>
-                <IconButton aria-label="mute user">
-                  <VolumeOffIcon />
-                </IconButton>
+  )
+}
+
+
+function Sheet({className= ""}){
+  return (
+    <div className="sheet">
+      <div className="controls">
+        <IconButton aria-label="close morse view">
+          <CloseIcon />
+        </IconButton>
+      </div>
+      <div className="list">
+        <p>A .-</p><p>B -...</p><p>C -.-.</p><p>D -..</p>
+        <p>E .</p><p>F ..-.</p><p>G --.</p><p>H ....</p>
+        <p>I ..</p><p>J .---</p><p>K -.-</p><p>L .-..</p>
+        <p>M --</p><p>N -.</p><p>O ---</p><p>P .--.</p>
+        <p>Q --.-</p><p>R .-.</p><p>S ...</p><p>T -</p>
+        <p>U ..-</p><p>V ...-</p><p>W .--</p><p>X -..-</p>
+        <p>Y -.--</p><p>Z --..</p><p>. .-.-.-</p><p>, --..--</p>
+        <p>? ..--..</p><p>' .----.</p><p>! -.-.--</p><p>/ -..-.</p>
+        <p>: ---...</p><p>; -.-.-.</p><p>= -...-</p><p>+ .-.-.</p>
+        <p>- -....-</p><p>@ .--.-.</p><p>1 .----</p><p>2 ..---</p>
+        <p>3 ...--</p><p>4 ....-</p><p>5 .....</p><p>6 -....</p>
+        <p>7 --...</p><p>8 ---..</p><p>9 ----.</p><p>0 -----</p>
+
+      </div>
+    </div>
+  );
+}
+
+function Preview({className= ""}){
+  return (
+    <div className="preview">
+      <div className="progress">
+      </div>
+      <div className="text">
+        <p>hello world cammin di nostra vita mi ritrovai per una selva uscura_..</p>
+        <IconButton aria-label="cancel message">
+          <DeleteOutlineIcon />
+        </IconButton>
+      </div>
+
+    </div>
+  );
+}
+
+function Chat({className= ""}){
+  return (
+    <div className="chat">
+      { /* typed stuff, progress bar, actual chat*/}
+      <p><span>IT00HAL</span>: asdasd asd dsad ads</p>
+      <p><span>IT00HAL</span>: asdasd asd dsad ads</p>
+      <p><span>IT00HAL</span>: asdasd asd dsad ads</p>
+      <p><span>IT00HAL</span>: asdasd asd dsad ads</p>
+      <p><span>IT00HAL</span>: asdasd asd dsad ads</p>
+      <p><span>IT00HAL</span>: asdasd asd dsad ads</p>
+      <p className="you"><span>IT00HAL</span>: asdasd asd dsad ads</p>
+      <p><span>IT00HAL</span>: asdasd asd dsad ads</p>
+      <p><span>IT00HAL</span>: asdasd asd dsad ads</p>
+      <p><span>IT00HAL</span>: asdasd asd dsad ads</p>
+      <p><span>IT00HAL</span>: asdasd asd dsad ads</p>
+      <p><span>IT00HAL</span>: asdasd asd dsad ads</p>
+      <p><span>IT00HAL</span>: asdasd asd dsad ads</p>
+    </div>
+  );
+}
+
+function Key({className= ""}){
+  return (
+    <div className="key">
+      <button className='activex'>-</button>
+    </div>
+  );
+}
+function Online({className= ""}){
+  return (
+    <div className="online">
+      <h2>online</h2>
+      {
+        ['IT000HAL', 'AS89ASD', 'ASDASDd', 'SLUR000'].map((h, i) =>
+          <div key={i}>
+            <div className="left">
+              <p>{h}</p>
+              <div className="typing">
+                <div></div>
+                <div></div>
               </div>
-              )
-          }
+            </div>
+            <IconButton aria-label="mute user">
+              <VolumeOffIcon />
+            </IconButton>
           </div>
+        )
+      }
+    </div>
+  );
+}
+function SideControls({className= ""}){
+  return (
+    <div className="sidecontrols">
+      <h2>controls</h2>
+      <p>wpm</p>
+      <p>receiver volume</p>
+      <p>key volume</p>
+      <p>submit delay</p>
+      <p>show words</p>
+      <Button size="small" startIcon={<SettingsIcon />} variant="outlined">
+        Advanced
+      </Button>
+    </div>
+  );
+}
 
-          <div className="sidecontrols">
-            <h2>controls</h2>
-            <p>wpm</p>
-            <p>receiver volume</p>
-            <p>key volume</p>
-            <p>submit delay</p>
-            <p>show words</p>
-            <Button size="small" startIcon={<SettingsIcon/>} variant="outlined">
-              Advanced
-            </Button>
-          </div>
-      </div>
-      <div className="preview">
-        <div className="progress">
+export default function App() {
+  return (
+    <div className="app-container">
+      <Header />
+      <main>
+        <Info className="grid-info" />
+        <div className="side">
+          <Online />
+          <SideControls />
         </div>
-        <div className="text">
-          <p>hello world cammin di nostra vita mi ritrovai per una selva uscura_..</p>
-          <IconButton aria-label="cancel message">
-            <DeleteOutlineIcon />
-          </IconButton>
-        </div>
-
-      </div>
-      <div className="chat">
-          { /* typed stuff, progress bar, actual chat*/ }
-          <p><span>IT00HAL</span>: asdasd asd dsad ads</p>
-          <p><span>IT00HAL</span>: asdasd asd dsad ads</p>
-          <p><span>IT00HAL</span>: asdasd asd dsad ads</p>
-          <p><span>IT00HAL</span>: asdasd asd dsad ads</p>
-          <p><span>IT00HAL</span>: asdasd asd dsad ads</p>
-          <p><span>IT00HAL</span>: asdasd asd dsad ads</p>
-          <p className="you"><span>IT00HAL</span>: asdasd asd dsad ads</p>
-          <p><span>IT00HAL</span>: asdasd asd dsad ads</p>
-          <p><span>IT00HAL</span>: asdasd asd dsad ads</p>
-          <p><span>IT00HAL</span>: asdasd asd dsad ads</p>
-          <p><span>IT00HAL</span>: asdasd asd dsad ads</p>
-          <p><span>IT00HAL</span>: asdasd asd dsad ads</p>
-          <p><span>IT00HAL</span>: asdasd asd dsad ads</p>
-      </div>
-      <div className="key">
-        <button className='activex'>-</button>
-      </div>
-      <div className="sheet">
-        <div className="controls">
-          <IconButton aria-label="close morse view">
-            <CloseIcon />
-          </IconButton>
-        </div>
-        <div className="list">
-          <p>A .-</p><p>B -...</p><p>C -.-.</p><p>D -..</p>
-          <p>E .</p><p>F ..-.</p><p>G --.</p><p>H ....</p>
-          <p>I ..</p><p>J .---</p><p>K -.-</p><p>L .-..</p>
-          <p>M --</p><p>N -.</p><p>O ---</p><p>P .--.</p>
-          <p>Q --.-</p><p>R .-.</p><p>S ...</p><p>T -</p>
-          <p>U ..-</p><p>V ...-</p><p>W .--</p><p>X -..-</p>
-          <p>Y -.--</p><p>Z --..</p><p>. .-.-.-</p><p>, --..--</p>
-          <p>? ..--..</p><p>' .----.</p><p>! -.-.--</p><p>/ -..-.</p>
-          <p>: ---...</p><p>; -.-.-.</p><p>= -...-</p><p>+ .-.-.</p>
-          <p>- -....-</p><p>@ .--.-.</p><p>1 .----</p><p>2 ..---</p>
-          <p>3 ...--</p><p>4 ....-</p><p>5 .....</p><p>6 -....</p>
-          <p>7 --...</p><p>8 ---..</p><p>9 ----.</p><p>0 -----</p>
-
-        </div>
-      </div>
-      <div className="info">
-        <h1>Morsechat</h1>
-        <p>Welcome to the best internet morse radio. </p>
-        <p>
-          This webapp is open source! <br/> Join the discord community
-          to get the latest news, and to participate on its development.
-        </p>
-        <h3>What's new</h3>
-        <p>2021-02-10 update notes</p>
-        <a href="https://discord.gg/JPzfzNJG6e">
-          <img src={discordUrl} />
-        </a><br/>
-        <a href="https://github.com/robalb/morsechat">
-          <img src={githubUrl} />
-        </a>
-      </div>
-
-
-
-    </main>
+        <Preview />
+        <Chat />
+        <Key />
+        <Sheet />
+      </main>
     </div>
   );
 }
