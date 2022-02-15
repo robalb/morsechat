0f10e78ce0ad8fa34f802f3019faad2190a717b9
robalb <alberto.ventafridda@gmail.com>
2022-02-15 17:08:11 +0100

    added submenu with working logout button to header loggedicon

--- morse-vite/src/components/header/Header.jsx
+++ morse-vite/src/components/header/Header.jsx
@@ -9,9 +9,19 @@ import Skeleton from "@mui/material/Skeleton"
 import MenuItem from "@mui/material/MenuItem";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import Logout from '@mui/icons-material/Logout';
+import ListItemIcon from '@mui/material/ListItemIcon';
 
 export function Header({leftContent = "", authState}) {
-    let {state} = React.useContext(mainContext)
+    let {state, post, reload} = React.useContext(mainContext)
+
+    async function logoutHandler(){
+        const res = await post('logout', {});
+        if(res.success){
+            reload()
+        }
+    }
 
     function setAuthPageClick(page){
         return e => {
@@ -19,12 +29,49 @@ export function Header({leftContent = "", authState}) {
         }
     }
 
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     let rightContent = 
         <Skeleton variant="rectangular" animation="wave" width={110} height={20} sx={{ bgcolor: 'grey.800' }}/>
 
     if(!state.loading){
         rightContent = state.sessionData.authenticated ?
-            <Button startIcon={<AccountCircleIcon />} >{state.userData.username}</Button>
+            <>
+                <Button
+                    startIcon={<AccountCircleIcon />}
+                    onClick={handleClick}
+                    aria-controls={open ? 'account-menu' : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? 'true' : undefined}
+                > {state.userData.username}</Button>
+                <Menu
+                    anchorEl={anchorEl}
+                    id="account-menu"
+                    open={open}
+                    onClose={handleClose}
+                    onClick={handleClose}
+                >
+                    <MenuItem>
+                        <ListItemIcon>
+                            <AccountCircleIcon fontSize="small" />
+                        </ListItemIcon>
+                        view profile
+                    </MenuItem>
+                    <MenuItem onClick={logoutHandler}>
+                        <ListItemIcon>
+                            <Logout fontSize="small" />
+                        </ListItemIcon>
+                        Logout
+                    </MenuItem>
+                </Menu>
+            </>
             :
             <>
                 <Button onClick={setAuthPageClick("login")}>Login</Button>
