65bf8b97b7872b810619c197df0b457cdd6a70ca
robalb <alberto.ventafridda@gmail.com>
2022-02-15 18:22:45 +0100

    added slider controls, connected the first one to the appctx, playing with react optimization

--- morse-vite/src/components/sideControls/SideControls.jsx
+++ morse-vite/src/components/sideControls/SideControls.jsx
@@ -1,17 +1,35 @@
 import Button from "@mui/material/Button";
 import SettingsIcon from "@mui/icons-material/Settings";
 import * as React from "react";
+import Slider from '@mui/material/Slider';
 
+import appContext from "../../contexts/appContext";
 import styles from './sideControls.module.css';
 
 export function SideControls({className = ""}) {
+    let {settings, settingsDispatch} = React.useContext(appContext);
+    let [wpm, setWpm] = React.useState(settings.wpm)
     return (
         <div className={`${styles.sidecontrols} ${className}`}>
             <h2>controls</h2>
             <p>wpm</p>
+            <Slider size="small" value={wpm} aria-label="Default" valueLabelDisplay="auto" 
+                onChange={ (e, value) => setWpm(value) }
+                onBlur={e => 
+                    settingsDispatch({
+                        type: "update",
+                        payload: {
+                            wpm: wpm
+                        }
+                    })
+                }
+            />
             <p>receiver volume</p>
+            <Slider size="small" defaultValue={50} aria-label="Default" valueLabelDisplay="auto" />
             <p>key volume</p>
+            <Slider size="small" defaultValue={50} aria-label="Default" valueLabelDisplay="auto" />
             <p>submit delay</p>
+            <Slider size="small" defaultValue={50} aria-label="Default" valueLabelDisplay="auto" />
             <p>show words</p>
             <Button size="small" startIcon={<SettingsIcon/>} variant="outlined">
                 Advanced
