0a5603c3ee7ba7b34467581b0d160200d4edd98f
robalb <alberto.ventafridda@gmail.com>
2022-02-28 22:08:29 +0100

    animation status is now animated

--- morse-vite/src/components/online/Online.jsx
+++ morse-vite/src/components/online/Online.jsx
@@ -5,15 +5,36 @@ import * as React from "react";
 import styles from './online.module.css';
 
 export function Online({className = "", connectionStatus}) {
-    let status = connectionStatus ? "" : "- connecting.."
+    let status = connectionStatus ? "connected" : "connecting.."
+
+    const [animationClass, setAnimationClass] = React.useState(true);
+    const [firstRender, setFirstRender] = React.useState(true);
+
+    //change the animation class when the currentAction text changes
+    React.useEffect(() => {
+        //avoid showing the animation on the first render of the component
+        if (firstRender) {
+            setFirstRender(false)
+        } else {
+            setAnimationClass(a => !a)
+        }
+    }, [connectionStatus]);
+
+
+    let statusStyles = [
+        styles.status,
+        (animationClass ? styles.a_1 : styles.a_2),
+        (connectionStatus ? styles.ws_online : null)
+
+    ].join(" ")
     return (
         <div className={`${styles.online_container} ${className}`}>
-            <p className={`${styles.status} ${styles.a_1}`} >connecting</p>
+            <p className={statusStyles} >{status}</p>
             <hr />
-            <h2>online users {status}</h2>
+            <h2>online users </h2>
             <div className={styles.online}>
                 {
-                    Array(10).fill('ASDASD').concat(['FINAL00']).map((h, i) =>
+                    Array(5).fill('ASDASD').concat(['FINAL00']).map((h, i) =>
                         <div key={i}>
                             <div className={styles.left}>
                                 <p>{h}</p>
@@ -23,7 +44,7 @@ export function Online({className = "", connectionStatus}) {
                                 </div>
                             </div>
                             <IconButton aria-label="mute user">
-                                <VolumeOffIcon/>
+                                <VolumeOffIcon />
                             </IconButton>
                         </div>
                     )
