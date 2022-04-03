992421483a81af8d4ad93254cd6f53b1ca457e1e
robalb <alberto.ventafridda@gmail.com>
2022-04-04 00:54:05 +0200

    reached a working but untested state for the yambic system

--- web/src/components/key/Key.jsx
+++ web/src/components/key/Key.jsx
@@ -37,25 +37,79 @@ function KeyInternal(props){
   }, [])
 
 
-  const SPACE_DOT = "space"
-  const SPACE_DASH = "space"
+  //the space after a dot
+  const SPACE_DOT = "space_dot"
+  //the space after a dash
+  const SPACE_DASH = "space_dash"
   const DOT = "dot"
   const DASH = "dash"
+  
+  //TODO: get this from the settings
+  const times = {
+    [SPACE_DOT]: 500,
+    [SPACE_DASH]: 500,
+    [DOT]: 500,
+    [DASH]: 1500
+  }
+
+  function scheduleYambicEvent(event){
+    if(interval.current){
+      console.log("WARNING weird state: setting timer while another is running")
+    }
+    else{
+      interval.current = setTimeout(()=>{ setYambicEvent(event) }, times[event])
+    }
+  }
 
   //start the timer
   React.useEffect(()=>{
-    if(!interval.current && (dotDown || dashDown)){
-      console.log("starting")
-      setTimeout(()=>{
-        setYambicEvent(DASH)
-      }, 2000)
+    console.log("starting")
+    if(!interval.current){
+      if(dotDown && dashDown){
+        console.log("WARNING weird state: bot dot and dash down, no interval running")
+      }
+      else if(dotDown){
+        down()
+        interval.current = setTimeout(()=>{ setYambicEvent(DOT) }, times[DOT])
+      }
+      else if(dashDown){
+        down()
+        interval.current = setTimeout(()=>{ setYambicEvent(DASH) }, times[DASH])
+      }
     }
   }, [dotDown, dashDown])
 
-  //timer stopped
+
+  //react to a timer stop
   React.useEffect(()=>{
-    console.log("event terminated:")
+    // console.log("event terminated:")
     console.log(yambicEvent)
+    interval.current = null
+    //one of the following events has terminated
+    if(yambicEvent === DOT){
+      up()
+      interval.current = setTimeout(()=>{ setYambicEvent(SPACE_DOT) }, times[SPACE_DOT])
+    }
+    else if(yambicEvent === DASH){
+      up()
+      interval.current = setTimeout(()=>{ setYambicEvent(SPACE_DASH) }, times[SPACE_DASH])
+    }
+    else{
+      if(dotDown && dashDown){
+        down()
+        const alternate = (yambicEvent == SPACE_DASH ? DOT : DASH)
+        interval.current = setTimeout(()=>{ setYambicEvent(alternate) }, times[alternate])
+      }
+      else if(dotDown){
+        down()
+        interval.current = setTimeout(()=>{ setYambicEvent(DOT) }, times[DOT])
+      }
+      else if(dashDown){
+        down()
+        interval.current = setTimeout(()=>{ setYambicEvent(DASH) }, times[DASH])
+      }
+
+    }
   }, [yambicEvent])
 
   function yambicDown(isDot){
@@ -81,7 +135,7 @@ function KeyInternal(props){
     else if(keyMode === "yambic"){
       let isLeft = e.nativeEvent.which == 1
       let isDot = (isLeft == leftIsDot) //xor logic operation
-      yambicDown(isLeft)
+      yambicDown(isDot)
     }
   }
 
@@ -92,16 +146,16 @@ function KeyInternal(props){
     else if(keyMode === "yambic"){
       let isLeft = e.nativeEvent.which == 1
       let isDot = (isLeft == leftIsDot) //xor logic operation
-      yambicUp(isLeft)
+      yambicUp(isDot)
     }
   }
 
   function down(e){
-    console.log("down")
+    console.log(">>down")
   }
 
   function up(e){
-    console.log("up")
+    console.log(">>up")
   }
 
 
