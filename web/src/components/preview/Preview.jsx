907dd901bc5c153f6f3994f7abe07fd26b012fb6
robalb <alberto.ventafridda@gmail.com>
2022-04-06 19:59:05 +0200

    simple dit-dash keyer visualization in preview

--- web/src/components/preview/Preview.jsx
+++ web/src/components/preview/Preview.jsx
@@ -8,8 +8,59 @@ import { resetMessage } from "../../redux/chatSlice";
 
 function PreviewInternal(){
     let buffer = useSelector(state => state.chat.messageBuffer)
-    let text = buffer.join(" ")
-    return <p>{text}</p>
+    let wpm = useSelector(state => state.user.settings.wpm)
+
+
+    //TODO: abstract this into some sort of reusable function and put it somewhere else
+    function getTimes(wpm){
+        if(wpm < 1) wpm = 1
+        if(wpm > 200) wpm = 200
+        const dotTime = Math.ceil(1200 / wpm)
+        return {
+            dot: dotTime,
+            dash: dotTime * 3,
+            elementGap: dotTime,
+            letterGap: dotTime * 3,
+            wordGap: dotTime * 7
+        }
+    }
+    let times = getTimes(wpm)
+    // console.log(times)
+    function bufferedMorseToString(buffer, times){
+        let out = ""
+        let down = true;
+        for(let i=0; i<buffer.length; i++){
+            // console.log("--- " + i)
+            let t = buffer[i]
+            //released after t millis
+            if(down){
+                // console.log("released after " + t)
+                if(t < times.dash){
+                    out += "."
+                }else{
+                    out += "_"
+                }
+            }
+            //pressed after t millis
+            else{
+                // console.log("pressed after " + t)
+                if(t > times.wordGap){
+                    out+="/"
+                }
+                else if(t > times.letterGap){
+                    out += " "
+                }
+                else{
+                    out += ""
+                }
+            }
+            down = !down
+        }
+        return out
+    }
+    let morseString = bufferedMorseToString(buffer, times)
+
+    return <p>{morseString}</p>
 }
 
 export function Preview({className = ""}) {
