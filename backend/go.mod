d6d7ed0337d72fd6442198dc0c3dd2af0a25d7da
robalb <11277482+robalb@users.noreply.github.com>
2024-11-27 11:22:53 +0100

    Add chi middleware

--- backend/go.mod
+++ backend/go.mod
@@ -5,3 +5,5 @@ go 1.23.3
 require github.com/gorilla/websocket v1.5.3
 
 require github.com/go-chi/chi/v5 v5.1.0
+
+require github.com/go-chi/cors v1.2.1
--- backend/server.go
+++ backend/server.go
@@ -6,8 +6,8 @@ import (
 
   "github.com/go-chi/chi/v5"
   "github.com/go-chi/chi/v5/middleware"
-	// "github.com/robalb/morsechat/middleware"
-
+  "github.com/go-chi/cors"
+	localmiddleware "github.com/robalb/morsechat/middleware"
 )
 
 func newServer(
@@ -18,7 +18,21 @@ func newServer(
   ) http.Handler{
 
   mux := chi.NewRouter()
+  mux.Use(localmiddleware.RealIPFromHeaders("X-Forwarded-For"))
   mux.Use(middleware.Logger)
+  mux.Use(middleware.Recoverer)
+  mux.Use(middleware.Heartbeat("/health"))
+  mux.Use(cors.Handler(cors.Options{
+    // AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+    AllowedOrigins:   []string{"https://*", "http://*"},
+    // AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+    AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+    AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+    ExposedHeaders:   []string{"Link"},
+    AllowCredentials: false,
+    MaxAge:           300, // Maximum value not ignored by any of major browsers
+  }))
+
   AddRoutes(
     mux,
     logger,
